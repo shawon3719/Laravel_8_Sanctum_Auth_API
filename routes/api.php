a3b792fc5b64a60dd9306843a978a4c8b3bd422a
shawon3719 <shawon3719@gmail.com>
2021-02-07 23:08:00 +0600

    created login and register using sanctum token

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', 'App\Http\Controllers\userController@logout')->name('logout.api');
+    Route::get('/user', 'App\Http\Controllers\userController@userdata')->name('user.api');
 });
+
+
+Route::post('/register', 'App\Http\Controllers\Auth\registerController@register');
+Route::post('/login', 'App\Http\Controllers\Auth\loginController@login');
